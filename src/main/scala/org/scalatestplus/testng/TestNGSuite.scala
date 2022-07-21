d1b354405c4fc2da21d077b63888e950be75b8cf
Chua Chee Seng <cheeseng@amaseng.com>
2022-07-21 21:35:44 +0800

    Changed to use @BeforeMethod for example in scaladoc.

--- src/main/scala/org/scalatestplus/testng/TestNGSuite.scala
+++ src/main/scala/org/scalatestplus/testng/TestNGSuite.scala
@@ -25,7 +25,7 @@ package org.scalatestplus.testng
  * <pre class="stHighlight">
  * import org.scalatest.testng.TestNGSuite
  * import org.testng.annotations.Test
- * import org.testng.annotations.Configuration
+ * import org.testng.annotations.BeforeMethod
  * import scala.collection.mutable.ListBuffer
  * 
  * class MySuite extends TestNGSuite {
@@ -33,8 +33,7 @@ package org.scalatestplus.testng
  *   var sb: StringBuilder = _
  *   var lb: ListBuffer[String] = _
  * 
- *   @Configuration(beforeTestMethod = true)
- *   def setUpFixture() {
+ *   @BeforeMethod def setUpFixture() {
  *     sb = new StringBuilder("ScalaTest is ")
  *     lb = new ListBuffer[String]
  *   }
